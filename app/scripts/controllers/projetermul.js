d20eff16ebf6b2fdfe7b23caeedfcac17ef3997c
Whispyy <jfdurk@hotmail.fr>
2015-12-01 10:13:05 +0100

    debut projetermul

--- app/scripts/controllers/projetermul.js
+++ app/scripts/controllers/projetermul.js
@@ -8,10 +8,30 @@
  * Controller of the maquetteGlApp
  */
 angular.module('maquetteGlApp')
-  .controller('ProjetermulCtrl', function () {
+  .controller('ProjetermulCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.msg ='Projeter Multiple';
+
+    // gestion étape 1
+    $scope.file='';
+    // gestion étape 2
+
+
+    // étapes avant la projection
+    $scope.step1 = true;
+    $scope.step2 = false;
+    $scope.next = function(){
+  		$scope.step1 =false;
+  		$scope.step2 =true;
+      console.log($scope.file);
+    };
+   	$scope.back = function(){
+  		$scope.step1 = true;
+  		$scope.step2 = false;
+    };
   });
